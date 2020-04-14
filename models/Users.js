3875ab36c6fe97728e2ad7e23c620299270aca49
Peter Rose <52871421+PeterTheDeveloper@users.noreply.github.com>
2020-04-14 15:19:09 -0400

    User updated

--- models/Users.js
+++ models/Users.js
@@ -1,35 +1,41 @@
 const db = require('../db');
 
 class User {
-    static addUser(first_name, last_name, email, password, industry, job_position, job_description, thumbs_up, Contact_info) {
-      const queryText = `INSERT INTO users (first_name, last_name, email, password, industry, job_position, 
+  static addUser(first_name, last_name, email, password, industry, job_position, job_description, thumbs_up, Contact_info) {
+    const queryText = `INSERT INTO users (first_name, last_name, email, password, industry, job_position, 
         job_description, thumbs_up, Contact_info) VALUES ($1, $2, $3, $4, $5, $6, $7, $8. $9)`;
-    }
-    static addConnection() {
-
-    }
-    static getByEmail(email) {
-      const queryText = 'SELECT * FROM users WHERE email = $1;';
-      return db.query(queryText, [email])
-          .then((data) => data.rows[0]);
   }
-    static getConnections(sender_id) {
-        const queryText = `SELECT connections.reciever_id from connections WHERE conenctions.sender_id = $1 AND connected = TRUE`;
-        return db.query(queryText, [sender_id])
-            .then((data) => {
-              data.rows.forEach((reciever) => {
-                getById(reciever)
-              });
-            });
-    }
+  static addConnection() {
+    const queryText = ``;
+  }
 
-    static updateUser(id, first_name, last_name, email, industry, job_position, job_description, Contact_info) {
-      const queryText = `UPDATE users SET first_name = $2, last_name = $3, email = $4, industry = $4, job_position = $5, Contact_info = $6
+  static getByEmail(email) {
+    const queryText = 'SELECT * FROM users WHERE email = $1;';
+    return db.query(queryText, [email])
+      .then((data) => data.rows[0]);
+  }
+  static getById(id) {
+    const queryText = 'SELECT * FROM users WHERE id = $1;';
+    return db.query(queryText, [email])
+      .then((data) => data.rows[0]);
+  }
+  static getConnections(sender_id) {
+    const queryText = `SELECT connections.reciever_id from connections WHERE conenctions.sender_id = $1 AND connected = TRUE`;
+    return db.query(queryText, [sender_id])
+      .then((data) => {
+        data.rows.forEach((reciever) => { // Your connections already stored in user obj.
+          getById(reciever)
+        });
+      });
+  }
+
+  static updateUser(id, first_name, last_name, email, industry, job_position, job_description, Contact_info) {
+    const queryText = `UPDATE users SET first_name = $2, last_name = $3, email = $4, industry = $4, job_position = $5, Contact_info = $6
       WHERE id = $1`
-    }
-    static updatePassword() {
+  }
+  static updatePassword() {
 
-    }
+  }
 }
 
 module.exports = User;
