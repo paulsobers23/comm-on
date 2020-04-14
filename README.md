17a0af5b304b89dcf23333a13fc81f9080e2aabd
Peter Rose <52871421+PeterTheDeveloper@users.noreply.github.com>
2020-04-14 16:50:46 -0400

    ReadMe updated, Connections Methods updated

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# coffee-chat
+# coffee-chats
+A full stack web application allowing apiring/junior level software engineers to connect with high level engineers in a world where everything is virtual.
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* - NodeJs
+* - Express
+* - ESLint
+* - 
+* [Features](#features)
--- models/Users.js
+++ models/Users.js
@@ -1,15 +1,16 @@
+const bcrypt = require('bcrypt');
 const db = require('../db');
-const bcrypt = require('bcrypt')
 
 class User {
   static addUser(first_name, last_name, email, password, industry, job_position, job_description, thumbs_up, Contact_info) {
     const queryText = `INSERT INTO users (first_name, last_name, email, password, industry, job_position, 
         job_description, thumbs_up, Contact_info) VALUES ($1, $2, $3, $4, $5, $6, $7, $8. $9);`;
   }
+
   static addConnection(sender_id, reciever_id) {
-    const queryText = `INSERT INTO connections (sender_id, reciever_id, connected) VALUES ($1, $2);`;
+    const queryText = 'INSERT INTO connections (sender_id, reciever_id, connected) VALUES ($1, $2);';
     return db.query(queryText, [sender_id, reciever_id])
-      .then(data => console.log('Connection has been added:', data));
+      .then((data) => console.log('Connection has been added:', data));
   }
 
   static getByEmail(email) {
@@ -25,7 +26,7 @@ class User {
   }
 
   static getConnections(sender_id) {
-    const queryText = `SELECT connections.reciever_id from connections WHERE conenctions.sender_id = $1 AND connected = TRUE`;
+    const queryText = 'SELECT connections.reciever_id from connections WHERE conenctions.sender_id = $1 AND connected = TRUE';
     return db.query(queryText, [sender_id])
       .then((data) => {
         data.rows.forEach((reciever) => User.getById(sender_id)); // Your connections already stored in user obj.
@@ -34,8 +35,9 @@ class User {
 
   static updateUser(id, first_name, last_name, email, industry, job_position, job_description, Contact_info) {
     const queryText = `UPDATE users SET first_name = $2, last_name = $3, email = $4, industry = $4, job_position = $5, Contact_info = $6
-      WHERE id = $1`
+      WHERE id = $1`;
   }
+
   static updatePassword() {
     // This is immediately needed, Complete if needed, otherwise delete.
   }
