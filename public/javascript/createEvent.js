feda502bf83892a2b940340e63352040c631f426
Carmen Salas <carmensalas@Carmens-MacBook-Pro.local>
2020-04-19 21:58:21 -0400

    fixed createEvent file getting dateTime, and dateCreated

--- public/javascript/createEvent.js
+++ public/javascript/createEvent.js
@@ -18,29 +18,37 @@ const form = document.getElementById('createEvent');
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
   const title = form.title.value;
-  const date_time = form.date_time.value;
+  const dateTime = form.date_time.value;
   const purpose = form.purpose.value;
   const location = form.location.value;
   const description = form.description.value;
   const type = form.type.value;
-  const date_created = new Date().getTime;
+  const dateCreated = new Date();
 
-
-  console.log(date_created, date_time, title, description, purpose, location, type);
+  console.log(
+    dateCreated,
+    dateTime,
+    title,
+    description,
+    purpose,
+    location,
+    type,
+  );
   const request = await requestMethod('GET', '/events');
   const data = await request.json();
   console.log(data);
   const { creator } = data;
 
   console.log(creator);
-  const response = requestMethod('POST', '/events',
-  {creator,
-    date_created,
+  const response = requestMethod('POST', '/events', {
+    creator,
+    dateCreated,
     title,
     description,
     purpose,
     location,
-    date_time,
-    type});
+    dateTime,
+    type,
+  });
   window.location.href = '/home';
 });
